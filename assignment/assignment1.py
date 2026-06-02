c9cac47e0049f8d18f3b1232a64a7bc936a39ff7
him25918 <sharmahimanshu0725@gmail.com>
2026-06-03 05:24:49 +0530

    Initial commit

---
+++ assignment/assignment1.py
@@ -0,0 +1,109 @@
+# Question 1
+# Difference between the data types
+print("---------------------Question 1------------------------------------")
+# 1. Integer(int) : This datatype is used to store integer value without any decimal point.
+# example: 
+a = 10
+print(a)
+print(type(a))
+
+# 2. Float : this datatype is used to store the  integer value with decimal point.
+# example 
+b= 10.5
+print(b)
+print(type(b))
+
+# 3. String(str) : this datatype store sequence character in double quotes, here the 
+# charactor can be alphanumeric as well as special symbols too.
+# ex 
+c = "Himanshu"
+print(c)
+print(type(c))
+
+# 4. Boolean(bool) : this datatype is used to store boolean values like yes or no or true and false.
+# ex 
+d = True
+print(d)
+print(type(d))
+
+
+
+#    Question 2
+print("------------------------------ Question 2 -------------------------------------------")
+name = "Himanshu"
+age = 20
+city = "pilani"
+print("Name: ",name)
+print("Age: ",age)
+print("City: ",city)
+
+
+# Question 3
+print("------------------------------ Question 3 -------------------------------------------")
+user_name = input("Enter your name : ")
+print(user_name.upper())
+print(len(user_name))
+
+# Question 4 
+print("------------------------------ Question 4 -------------------------------------------")
+# 5 String methods
+# 1. upper() : this method is used to convert the string to uppercase.
+abc = "pilani"
+print(abc.upper())
+
+# 2. lower() : this method is used to convert the string to lowercase.
+ab = "PILANI"
+print(ab.lower())
+
+# 3. replace() : this method is used to replace a word to another word in a String.
+a = "Hello world"
+print(a.replace("world","himanshu"))
+
+# 4. strip() : this method is used to remove the white spaces of the string in starting and ending.
+s = "   hello   "
+print(s.strip())
+
+# 5. title() : this method the converts the first letter of the word to capital
+print(abc.title())
+
+#   Question 5
+print("------------------------------ Question 5 -------------------------------------------")
+fruits = ["mango","banana","orange","grapes","kiwi"]
+print(fruits)
+print(fruits[0])
+print(fruits[4])
+print(len(fruits))
+
+# Question 6
+print("------------------------------ Question 6 -------------------------------------------")
+lst = [10,20,30,40,50]
+print(lst)
+lst.append(60)
+lst.remove(20)
+print(lst)
+
+
+# Question 7
+# Artificial intelligence(AI) : AI is the branch of computer science that studies to clone human intelligence
+# so it can make machines or computer do the work which required human intelligence.
+
+# Importance of AI :
+# 1. It saves time and man power
+# 2. High Accuracy
+# 3. Increased speed of work
+# 4. Automates unnecesary tasks
+
+# Real life applications of AI
+# 1. Virtual assistants : AI is used in them to answer our questions and perform tasks.
+# 2. Health care : Ai is used in health care to detech disease with symptoms and medical diagnosis.
+# 3. Self driving cars
+# 4. Recommentations systems
+
+
+# Question 8
+
+# 1. chatgpt is an example of AI as it understands and genrated human like texts using AI.
+# 2. Google Maps route prediction: also an example of Ai as it gives us shortest route based on our input.
+# 3. Calculator : Not an AI 
+# 4. Netflix recommendations : it uses Ai to recommend useful movies or web series of our liking based on previous history.
+# 5.  Voice assistants (Alexa/Siri): uses AI to give us answers and perform tasks.
---
+++ python/basic.py
@@ -0,0 +1,78 @@
+# name = "abc"
+# print("this is the string ", name)
+# print(type(name))
+# print(len(name))
+
+# temp = "45 days training"
+# print(temp[3:7])
+# print(temp[::-1])
+# print(temp.upper())
+
+# address = "pilani"
+# # print(address.lower())
+# # print(address.upper())
+# print(address.title()) # makes first letter capital 
+# #capitalize() also works the same it makes every first letter of word capital
+
+# name = "onngtnintnietinh"
+# print(name.count('n'))
+# print(name.index('n'))
+# print(name.find('n'))
+# split function
+
+
+# intro = "i love python"
+# print(intro.replace("python","java"))
+# print(intro)
+
+# a = "python"
+# print(a.startswith("p"))
+# print(a.endswith("p"))
+
+num = "124"
+char = "hello"
+a="hello123  "
+# print(num.isdigit())
+# print(char.isdigit())
+# print(char.isalpha())
+# print(char.isalnum())
+# print(char*3)
+# print(char+char)
+# print(a.strip())
+
+# name = "himanshu"
+# add = "pilani"
+# print(f"My name is {name} and i am from {add}") # formatting k liye string k aage f likhte hai
+
+
+
+
+# list == collection of heterogenous item , mutable, ordered
+
+lst = [1,2,3,"abc",6.89]
+# print(lst)
+# print(type(lst))
+# print(lst[2:5])
+# lst.append(5)
+# lst.insert(0,100)
+# lst.insert(1,"pilani")
+
+# lst.extend([1,2,3,4])
+# lst.append([1,2,3,4])
+# print(lst)
+# print(lst[5][1])
+# lst1 = lst
+# lst2 = lst.copy()
+# print(lst1)
+# print(lst2)
+# print(lst==lst1)
+# print(lst==lst2)
+# print(lst1 == lst2)
+
+lst1 = [1,2,4,5]
+lst2 = [3,4,5,6]
+print(lst1+lst2)
+print(lst1*2)
+# print(lst1*lst2)
+# print(lst1/lst2)
+# print(lst1//lst2)
